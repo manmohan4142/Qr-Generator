dd8ca839c8bccefea30d9e83d9a9c7923bbdb9de
Manmohan Dhal <96486531+manmohan4142@users.noreply.github.com>
2023-05-26 01:09:54 +0530

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -1,6 +1,6 @@
 import qrcode
-from pyzbar.pyzbar import decode
-from PIL import Image
+#from pyzbar.pyzbar import decode
+#from PIL import Image
 
 
 myqr = qrcode.make("https://www.youtube.com/@GateSmashers")
@@ -8,5 +8,5 @@ myqr1 = qrcode.make("https://www.youtube.com/@AnujBhaiya")
 myqr.save("myqr.png")
 myqr1.save("myqr1.png")
 
-b = decode(Image.open("myqr.png"))
-print(b[0].data.decode("ascii"))
+#b = decode(Image.open("myqr.png"))
+#print(b[0].data.decode("ascii"))
